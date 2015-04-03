9925285a98db529c50db6c0a8cde77eb5d2a92c4
Catalina Turlea <catalina.turlea@gmail.com>
2015-04-03 22:01:38 +0200

    Initial Commit

---
+++ HeaderWebView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeaderWebView
+//
+//  Created by Catalina Turlea on 4/3/15.
+//  Copyright (c) 2015 Catalina Turlea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HeaderWebView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HeaderWebView
+//
+//  Created by Catalina Turlea on 4/3/15.
+//  Copyright (c) 2015 Catalina Turlea. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
